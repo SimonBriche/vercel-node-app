976e39175f37bf4c01f7389994979b1b9957ee59
Simon BRICHE <simon.briche@onprenduncafe.com>
2021-11-12 17:46:36 +0100

    add proxy

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const https = require('https');
 const express = require('express');
+require('dotenv').config({silent: true});
 
 const app = express();
 
+
+/*
 app.get('/', (req, res) => res.send('Home Page Route'));
 
 app.get('/about', (req, res) => res.send('About Page Route'));
@@ -9,6 +13,16 @@ app.get('/about', (req, res) => res.send('About Page Route'));
 app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
 app.get('/contact', (req, res) => res.send('Contact Page Route'));
+*/
+
+app.get('*', (req, res) => {
+  const proxyURL = process.env.APPLICATION_URL+req.path;
+  const proxy = https.request(proxyURL, function (r) {
+    res.writeHead(r.statusCode, r.headers);
+    r.pipe(res, {end: true});
+  });
+  req.pipe(proxy, {end: true});
+});
 
 const port = process.env.PORT || 3000;
 
